7c39a4e7021c4f26a7ff04d511fec0c21b506e47
nedsi <32332697+nedsir@users.noreply.github.com>
2021-05-18 00:04:23 +0200

    first commit

---
+++ keymaps/nedsi/config.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#ifdef OLED_DRIVER_ENABLE
+  #define OLED_DISPLAY_128X64
+#endif
+
+#ifdef RGBLIGHT_ENABLE
+  #define RGBLIGHT_HUE_STEP 8
+  #define RGBLIGHT_SAT_STEP 8
+  #define RGBLIGHT_VAL_STEP 8
+#endif
+
+#ifdef ENCODER_ENABLE
+#    define ENCODER_DIRECTION_FLIP
+#    define ENCODER_RESOLUTION 2
+#endif
+
+#define NO_ACTION_MACRO
+#define TAPPING_TERM 170
+#define TAPPING_TERM_PER_KEY
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// This prevents accidental repeats of the tap-hold keys when typing quickly.
+#define TAPPING_FORCE_HOLD
+
+// This prevents short hold periods to be interpreted as individual taps when typing quickly.
+#define PERMISSIVE_HOLD
+
+#define COMBO_VARIABLE_LEN
+#define COMBO_COUNT COMBO_VARIABLE_LEN
+#define COMBO_TERM 70
+
+#define UNICODE_SELECTED_MODES UC_WINC
+/* #define UNICODE_KEY_LNX LCTL(LSFT(KC_E)) */
---
+++ keymaps/nedsi/rules.mk
@@ -0,0 +1,16 @@
+OLED_DRIVER_ENABLE = yes   # Enable the use of OLED displays
+ENCODER_ENABLE = yes       # Enable the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+SPLIT_TRANSPORT = mirror   # Allow to have information sent on both sides
+TAP_DANCE_ENABLE = yes     # Activate tap dance
+UNICODEMAP_ENABLE = yes    # Enable unicode map
+COMBO_ENABLE = yes 				 # Enable combos
+NKRO_ENABLE = yes 				 # Enable NKRO: any number of key-presses at once
+LTO_ENABLE = yes  				 # Enable Link Time Optimization
+MOUSEKEY_ENABLE = yes      # Enable mouse keys
+
+SRC += combos.c			       # Source combos.c 
+
+EXTRFLAGS += -flto
+
+WPM_ENABLE = no     	     # Disable WPM measurement
